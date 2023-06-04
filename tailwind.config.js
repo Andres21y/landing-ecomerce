11f16354ee63a9e54aa0d8c798b2c4d2b5d58e27
unknown <andres17vh@gmail.com>
2023-06-03 22:04:39 -0500

    fix: bug

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const { url } = require('inspector');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        flower: "url(.'./assets/img/desktop/flower)",
+        lake: "url(.'./assets/img/desktop/lake)",
+        logCabin: "url(.'./assets/img/desktop/logCabin)",
+        mountains: "url(.'./assets/img/desktop/mountains)",
+        mountainsG: "url(.'./assets/img/desktop/mountainsG)",
+        nature: "url(.'./assets/img/desktop/nature)",
+        tree: "url(.'./assets/img/desktop/tree)",
+        tree1: "url(.'./assets/img/desktop/tree1)",
+        tree2: "url(.'./assets/img/desktop/tree)2",
+        tree3: "url(.'./assets/img/desktop/tree)3",
+ },
+        // backgroundColor: (theme) => ({
+        //   ...theme("colors"),
+        //   primary: "#CC2D4A",
+        //   secondary: "#8FA206",
+        //   terciary: "#61AEC9",
+        // }),
+        // textColor: {
+        //   primary: "#CC2D4A",
+        //   secondary: "#8FA206",
+        //   terciary: "#61AEC9",
+        // },
+        // fontFamily: {
+        //   Montserrat: ["Montserrat", "sans-serif"],
+     // },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
+
